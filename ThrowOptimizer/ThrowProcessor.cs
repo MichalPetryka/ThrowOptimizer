e14fc1ff7abdf016da3413727ec852870f1c3c46
petris <paprikapolishgamer@gmail.com>
2023-02-04 01:18:09 +0100

    Add simple processing

--- ThrowOptimizer/ThrowProcessor.cs
+++ ThrowOptimizer/ThrowProcessor.cs
@@ -70,83 +70,183 @@ namespace ThrowOptimizer
 			if (!method.HasBody)
 				return;
 
-			if (method.Body.Instructions.All(instruction => instruction.OpCode.Code != Code.Throw) || method.Body.Instructions.All(instruction => instruction.OpCode.Code != Code.Ret))
+			if (method.Body.Instructions.All(instruction => instruction.OpCode.Code != Code.Throw))
 				return;
+
+			List<(int, int)> starts = ListPool<(int, int)>.Rent(first: (0, method.Body.Instructions.Count - 1));
+
+			for (int i = 0; i < method.Body.Instructions.Count; i++)
+			{
+				Instruction instruction = method.Body.Instructions[i];
+
+				switch (instruction.OpCode.Code)
+				{
+					//todo: try catch finally
+					case Code.Br or Code.Br_S:
+						starts.Add((i + 1, method.Body.Instructions.IndexOf((Instruction)instruction.Operand)));
+						break;
+					case Code.Brfalse or Code.Brfalse_S or Code.Brtrue or Code.Brtrue_S or Code.Beq or Code.Beq_S or Code.Bge or Code.Bge_S or Code.Bge_Un or Code.Bge_Un_S or Code.Bgt or Code.Bgt_S or Code.Bgt_Un or Code.Bgt_Un_S or Code.Ble or Code.Ble_S or Code.Ble_Un or Code.Ble_Un or Code.Blt or Code.Blt_S or Code.Blt_Un or Code.Blt_Un_S or Code.Bne_Un or Code.Bne_Un_S:
+						starts.Add((i + 1, method.Body.Instructions.IndexOf((Instruction)instruction.Operand)));
+						break;
+					case Code.Switch:
+						foreach (Instruction target in (IEnumerable<Instruction>)instruction.Operand)
+							starts.Add((i + 1, method.Body.Instructions.IndexOf(target)));
+						break;
+				}
+			}
+
+			if (starts.Count < 2)
+				return;
+
+			starts.Sort((left, right) => left.Item1.CompareTo(right.Item1));
 
 			Instruction[] body = method.Body.Instructions.ToArray();
 
 			foreach (int throwIndex in body.Select(instruction => instruction.OpCode.Code).ToArray().IndexOfAll(Code.Throw).Reverse())
 			{
-				// ReSharper disable once HeapView.BoxingAllocation
-				MethodDefinition throwHelper = new($"ThrowGeneratedException{method.Name}{throwIndex}", MethodAttributes.Static | MethodAttributes.Private, method.ReturnType)
+				int start = 0;
+
+				for (int i = starts.Count - 1; i >= 0; i--)
 				{
-					NoInlining = _configuration.NoInline,
-					Body =
+					(int, int) possibleStart = starts[i];
+
+					if (possibleStart.Item1 >= possibleStart.Item2)
 					{
-						InitLocals = true
+						return;
 					}
-				};
 
-				method.DeclaringType.Methods.Add(throwHelper);
+					if (possibleStart.Item2 == throwIndex)
+						continue;
 
-				Collection<Instruction> instructions = method.Body.Instructions;
-				List<int> starts = ListPool<int>.Rent(first: 0);
-				for (int i = 0; i < instructions.Count; i++)
-				{
-					Instruction instruction = instructions[i];
-
-					switch (instruction.OpCode.Code)
+					if (possibleStart.Item1 < throwIndex && possibleStart.Item2 < throwIndex &&
+						!new ArraySegment<Instruction>(body, possibleStart.Item1, possibleStart.Item2 - possibleStart.Item1)
+						.Any(c => c.OpCode.Code is Code.Throw or Code.Ret))
 					{
-						//todo: try catch finally
-						case Code.Br or Code.Br_S:
-							starts.Add(((Instruction)instruction.Operand).Offset);
-							break;
-						case Code.Brfalse or Code.Brfalse_S or
-							Code.Brtrue or Code.Brtrue_S or
-							Code.Beq or Code.Beq_S or
-							Code.Bge or Code.Bge_S or Code.Bge_Un or Code.Bge_Un_S or
-							Code.Bgt or Code.Bgt_S or Code.Bgt_Un or Code.Bgt_Un_S or
-							Code.Ble or Code.Ble_S or Code.Ble_Un or Code.Ble_Un or
-							Code.Blt or Code.Blt_S or Code.Blt_Un or Code.Blt_Un_S or
-							Code.Bne_Un or Code.Bne_Un_S:
-							starts.Add(instruction.Offset);
-							starts.Add(((Instruction)instruction.Operand).Offset);
-							break;
-						case Code.Switch:
-							starts.Add(instruction.Offset);
-							foreach (Instruction target in (Instruction[])instruction.Operand)
-								starts.Add(target.Offset);
-							break;
+						continue;
 					}
-				}
-				/*List<Instruction> instructions = new();
-				for (int i = throwIndex; i >= 0; i--)
-				{
-					if (method.Body.Instructions[i].OpCode.Code is Code.Br or Code.Br_S or
-						Code.Brfalse or Code.Brfalse_S or
-						Code.Brtrue or Code.Brtrue_S or
-						Code.Beq or Code.Beq_S or
-						Code.Bge or Code.Bge_S or Code.Bge_Un or Code.Bge_Un_S or
-						Code.Bgt or Code.Bgt_S or Code.Bgt_Un or Code.Bgt_Un_S or
-						Code.Ble or Code.Ble_S or Code.Ble_Un or Code.Ble_Un or
-						Code.Blt or Code.Blt_S or Code.Blt_Un or Code.Blt_Un_S or
-						Code.Bne_Un or Code.Bne_Un_S)
+
+					if (possibleStart.Item2 < throwIndex)
 					{
-						methodProcessor.InsertAfter(i, Instruction.Create(OpCodes.Call, throwHelper));
-						if (!method.ReturnType.Is(typeof(void)))
-							methodProcessor.InsertAfter(i + 1, Instruction.Create(OpCodes.Ret));
+						start = possibleStart.Item2;
 						break;
 					}
 
-					instructions.Add(method.Body.Instructions[i]);
-					methodProcessor.RemoveAt(i);
+					if (possibleStart.Item1 < throwIndex)
+					{
+						start = possibleStart.Item1;
+						break;
+					}
 				}
 
-				for (int i = instructions.Count - 1; i >= 0; i--)
-					throwProcessor.Append(instructions[i]);*/
+				if (start == 0)
+					continue;
+
+				MethodDefinition throwHelper = GenerateThrowHelper(method, new ArraySegment<Instruction>(body, start, throwIndex - start + 1), throwIndex);
+				method.DeclaringType.Methods.Add(throwHelper);
+				RewriteCallsite(method.Body, new ArraySegment<Instruction>(body, start, throwIndex - start + 1), throwHelper);
 			}
 
+			ListPool<(int, int)>.Return(starts);
 			method.Body.Optimize();
+		}
+
+		private MethodDefinition GenerateThrowHelper(MethodDefinition method, ArraySegment<Instruction> instructions, int id)
+		{
+			MethodDefinition throwHelper = new($"ThrowGeneratedException{method.Name}{id}", MethodAttributes.Static | MethodAttributes.Private, method.ReturnType)
+			{
+				NoInlining = _configuration.NoInline,
+				Body =
+				{
+					InitLocals = true
+				}
+			};
+			ILProcessor processor = throwHelper.Body.GetILProcessor();
+			int paramCounter = 0;
+
+			static Instruction GetLdArg(ref int counter)
+			{
+				return counter++ switch
+				{
+					0 => Instruction.Create(OpCodes.Ldarg_0),
+					1 => Instruction.Create(OpCodes.Ldarg_1),
+					2 => Instruction.Create(OpCodes.Ldarg_2),
+					3 => Instruction.Create(OpCodes.Ldarg_3),
+					<= byte.MaxValue => Instruction.Create(OpCodes.Ldarg_S, (byte)counter),
+					<= ushort.MaxValue => Instruction.Create(OpCodes.Ldarg, (ushort)counter),
+					_ => throw new Exception("Seriously?")
+				};
+			}
+			static int ToId(Instruction instruction)
+			{
+				return instruction.OpCode.Code switch
+				{
+					Code.Ldarg_0 or Code.Ldloc_0 => 0,
+					Code.Ldarg_1 or Code.Ldloc_1 => 1,
+					Code.Ldarg_2 or Code.Ldloc_2 => 2,
+					Code.Ldarg_3 or Code.Ldloc_3 => 3,
+					Code.Ldarg_S or Code.Ldloc_S or Code.Ldarga_S or Code.Ldloca_S => (byte)instruction.Operand,
+					Code.Ldarg or Code.Ldloc or Code.Ldarga or Code.Ldloca => (ushort)instruction.Operand,
+					_ => throw new Exception("Seriously?")
+				};
+			}
+			foreach (Instruction instruction in instructions)
+			{
+				switch (instruction.OpCode.Code)
+				{
+					case Code.Ldarg or Code.Ldarg_0 or Code.Ldarg_1 or Code.Ldarg_2 or Code.Ldarg_3 or Code.Ldarg_S:
+						throwHelper.Parameters.Add(new ParameterDefinition(method.Parameters[ToId(instruction)].ParameterType));
+						processor.Append(GetLdArg(ref paramCounter));
+						break;
+					case Code.Ldloc or Code.Ldloc_0 or Code.Ldloc_1 or Code.Ldloc_2 or Code.Ldloc_3 or Code.Ldloc_S:
+						throwHelper.Parameters.Add(new ParameterDefinition(method.Body.Variables[ToId(instruction)].VariableType));
+						processor.Append(GetLdArg(ref paramCounter));
+						break;
+					case Code.Ldarga or Code.Ldarga_S:
+						throwHelper.Parameters.Add(new ParameterDefinition(method.Parameters[ToId(instruction)].ParameterType.MakeByReferenceType()));
+						processor.Append(GetLdArg(ref paramCounter));
+						break;
+					case Code.Ldloca or Code.Ldloca_S:
+						throwHelper.Parameters.Add(new ParameterDefinition(method.Body.Variables[ToId(instruction)].VariableType.MakeByReferenceType()));
+						processor.Append(GetLdArg(ref paramCounter));
+						break;
+					default:
+						processor.Append(instruction);
+						break;
+				}
+			}
+
+			return throwHelper;
+		}
+
+		private void RewriteCallsite(MethodBody body, ArraySegment<Instruction> instructions, MethodDefinition method)
+		{
+			ILProcessor processor = body.GetILProcessor();
+			Instruction[] instr = instructions.Reverse().ToArray();
+
+			for (int i = 0; i < instr.Length; i++)
+			{
+				Instruction instruction = instr[i];
+				if (i == 0)
+				{
+					processor.InsertAfter(instruction, Instruction.Create(OpCodes.Ret));
+					processor.InsertAfter(instruction, Instruction.Create(OpCodes.Call, method));
+				}
+
+				switch (instruction.OpCode.Code)
+				{
+					case Code.Ldarg or Code.Ldarg_0 or Code.Ldarg_1 or Code.Ldarg_2 or Code.Ldarg_3 or Code.Ldarg_S:
+						break;
+					case Code.Ldloc or Code.Ldloc_0 or Code.Ldloc_1 or Code.Ldloc_2 or Code.Ldloc_3 or Code.Ldloc_S:
+						break;
+					case Code.Ldarga or Code.Ldarga_S:
+						break;
+					case Code.Ldloca or Code.Ldloca_S:
+						break;
+					default:
+						processor.Remove(instruction);
+						break;
+				}
+			}
 		}
 
 		public void Dispose()
